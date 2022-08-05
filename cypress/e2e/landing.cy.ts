01d8bf942944114a4bb7ca69eb9b1a5b29f86870
joseph-85 <109385169+joseph-85@users.noreply.github.com>
2022-08-05 01:00:40 -0500

    test: add main layout test

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -1,5 +1,8 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('test landing page', () => {
+  it('should show proper layout', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#header-id').exist()
+    cy.get('#main-id').exist()
+    cy.get('#footer-id').exist()
   })
 })
