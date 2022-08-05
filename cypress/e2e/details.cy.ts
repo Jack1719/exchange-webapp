c4f33e6a865b22ac4bc732d5d7ac81655ec94297
joseph-85 <109385169+joseph-85@users.noreply.github.com>
2022-08-05 02:52:01 -0500

    test: add details page

--- cypress/e2e/details.cy.ts
+++ cypress/e2e/details.cy.ts
@@ -1,5 +1,38 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('test details page', () => {
+  it('should show proper layout and allows to goback if we click back nav', () => {
+    cy.visit("http://localhost:3000/exchanges/binance")
+    cy.get('#header-id').should("exist")
+    cy.get('#main-id').should("exist")
+    cy.get('#footer-id').should("exist")
+    cy.get('#back-nav').click()
+    cy.location("pathname").should("eq", `/exchanges`)
+  }),
+  it('shoud show loading while fetching data and show warning', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://api.coingecko.com/api/v3/exchanges/binance',
+      status: 500,
+      delay: 1000,
+      response: 'fixture:binance.json',
+    }).as('getDetail')
+    cy.visit("http://localhost:3000/exchanges/binance")
+    cy.get('#loading-id').should("exist")
+    cy.wait('@getDetail')
+    cy.get('#warning-id').should('exist')
+  }),
+  it('shoud show loading and show proper detail', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://api.coingecko.com/api/v3/exchanges/binance',
+      status: 200,
+      delay: 1000,
+      response: 'fixture:binance.json',
+    }).as('getDetail')
+    cy.visit("http://localhost:3000/exchanges/binance")
+    cy.get('#loading-id').should("exist")
+    cy.wait('@getDetail')
+    cy.get('#detail-id').should('exist')
   })
 })
